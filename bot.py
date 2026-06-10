7ee364ac91cda64f02ca1815fd1f838f7ba148e1
minekanft-crypto <minekanft@gmail.com>
2026-06-10 15:47:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.reactions = True
 intents.members = True
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 active_spams = {}
 already_replied = set()
